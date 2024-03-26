3e7a856a33a1ce4a5825d6501f815e644c1dcddc
Nestorian <ananiev.nestor@ukr.net>
2024-03-26 11:01:18 +0200

    Add project files.

---
+++ Models/RouteStop.cs
@@ -0,0 +1,10 @@
+﻿namespace BusTicketingSystem.Models
+{
+    public class Stop
+    {
+        public int Id { get; set; } 
+        public GeoLocation? Location { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+    }
+}
