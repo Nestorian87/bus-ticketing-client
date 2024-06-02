8c8e218fda4742747f80c919a271421f0140b8d6
Nestorian <ananiev.nestor@ukr.net>
2024-06-02 07:33:17 +0300

    fix: text max length

--- Views/BusDataForm.Designer.cs
+++ Views/BusDataForm.Designer.cs
@@ -50,9 +50,11 @@
             modelComboBox.DisplayMember = "Name";
             modelComboBox.Font = new Font("Verdana", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 204);
             modelComboBox.FormattingEnabled = true;
-            modelComboBox.Location = new Point(217, 27);
+            modelComboBox.Location = new Point(134, 17);
+            modelComboBox.Margin = new Padding(2, 2, 2, 2);
+            modelComboBox.MaxLength = 30;
             modelComboBox.Name = "modelComboBox";
-            modelComboBox.Size = new Size(488, 40);
+            modelComboBox.Size = new Size(302, 28);
             modelComboBox.TabIndex = 30;
             modelComboBox.ValueMember = "Id";
             // 
@@ -66,10 +68,10 @@
             label3.AutoSize = true;
             label3.Font = new Font("Verdana", 10.875F, FontStyle.Regular, GraphicsUnit.Point, 204);
             label3.ForeColor = Color.FromArgb(52, 78, 65);
-            label3.Location = new Point(61, 28);
+            label3.Location = new Point(38, 18);
             label3.Margin = new Padding(0);
             label3.Name = "label3";
-            label3.Size = new Size(127, 35);
+            label3.Size = new Size(84, 23);
             label3.TabIndex = 29;
             label3.Text = "Модель";
             // 
@@ -82,9 +84,10 @@
             cancelButton.FlatStyle = FlatStyle.Flat;
             cancelButton.Font = new Font("Segoe UI", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 204);
             cancelButton.ForeColor = Color.White;
-            cancelButton.Location = new Point(282, 196);
+            cancelButton.Location = new Point(174, 122);
+            cancelButton.Margin = new Padding(2, 2, 2, 2);
             cancelButton.Name = "cancelButton";
-            cancelButton.Size = new Size(200, 57);
+            cancelButton.Size = new Size(123, 36);
             cancelButton.TabIndex = 32;
             cancelButton.Text = "Скасувати";
             cancelButton.UseVisualStyleBackColor = false;
@@ -99,9 +102,10 @@
             saveButton.FlatStyle = FlatStyle.Flat;
             saveButton.Font = new Font("Segoe UI", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 204);
             saveButton.ForeColor = Color.White;
-            saveButton.Location = new Point(505, 196);
+            saveButton.Location = new Point(311, 122);
+            saveButton.Margin = new Padding(2, 2, 2, 2);
             saveButton.Name = "saveButton";
-            saveButton.Size = new Size(200, 57);
+            saveButton.Size = new Size(123, 36);
             saveButton.TabIndex = 31;
             saveButton.Text = "Зберегти";
             saveButton.UseVisualStyleBackColor = false;
@@ -113,10 +117,10 @@
             Номер.AutoSize = true;
             Номер.Font = new Font("Verdana", 10.875F, FontStyle.Regular, GraphicsUnit.Point, 204);
             Номер.ForeColor = Color.FromArgb(52, 78, 65);
-            Номер.Location = new Point(61, 112);
+            Номер.Location = new Point(38, 70);
             Номер.Margin = new Padding(0);
             Номер.Name = "Номер";
-            Номер.Size = new Size(110, 35);
+            Номер.Size = new Size(72, 23);
             Номер.TabIndex = 33;
             Номер.Text = "Номер";
             // 
@@ -127,20 +131,21 @@
             numberMaskedTextBox.BackColor = Color.FromArgb(218, 215, 205);
             numberMaskedTextBox.BorderStyle = BorderStyle.FixedSingle;
             numberMaskedTextBox.Font = new Font("Verdana", 10.125F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            numberMaskedTextBox.Location = new Point(217, 112);
+            numberMaskedTextBox.Location = new Point(134, 70);
+            numberMaskedTextBox.Margin = new Padding(2, 2, 2, 2);
             numberMaskedTextBox.Mask = "LL 0000 LL";
             numberMaskedTextBox.Name = "numberMaskedTextBox";
-            numberMaskedTextBox.Size = new Size(488, 40);
+            numberMaskedTextBox.Size = new Size(301, 28);
             numberMaskedTextBox.TabIndex = 35;
             // 
             // BusDataForm
             // 
             AcceptButton = saveButton;
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(218, 215, 205);
             CancelButton = cancelButton;
-            ClientSize = new Size(737, 265);
+            ClientSize = new Size(458, 181);
             Controls.Add(numberMaskedTextBox);
             Controls.Add(Номер);
             Controls.Add(cancelButton);
@@ -148,7 +153,8 @@
             Controls.Add(modelComboBox);
             Controls.Add(label3);
             Icon = (Icon)resources.GetObject("$this.Icon");
-            MinimumSize = new Size(763, 336);
+            Margin = new Padding(2, 2, 2, 2);
+            MinimumSize = new Size(476, 228);
             Name = "BusDataForm";
             Text = "Автобус";
             ((System.ComponentModel.ISupportInitialize)busModelBindingSource).EndInit();
